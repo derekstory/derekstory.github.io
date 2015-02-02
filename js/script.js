6a45297242a5e9a82868715163f23a2ca2ffdf66
Story, Derek (DET-LCE) <Derek.Story@lowe-ce.com>
2015-02-02 10:47:48 -0500

    removed landscape prevention

--- js/script.js
+++ js/script.js
@@ -109,34 +109,7 @@ $(window).on('load',function () {
 
     
 
-$(window).bind('orientationchange resize', function(event){
-  if (event.orientation) {
-    if (event.orientation == 'landscape') {
-      if (window.rotation == 90) {
-        rotate(this, -90);
-      } else {
-        rotate(this, 90);
-      }
-    }
-  }
-});
-
-function rotate(el, degs) {
-  iedegs = degs/90;
-  if (iedegs < 0) iedegs += 4;
-  transform = 'rotate('+degs+'deg)';
-  iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
-  styles = {
-    transform: transform,
-    '-webkit-transform': transform,
-    '-moz-transform': transform,
-    '-o-transform': transform,
-    filter: iefilter,
-    '-ms-filter': iefilter
-  };
-  $(el).css(styles);
-}
-
+  
 
 
 });
